4a49353dec717403f25192255aec687eca8336e1
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-01-30 15:27:53 +1100

    Monkeypatch update to profile_line until skimage 0.10

--- lesion/profile.py
+++ lesion/profile.py
@@ -1,26 +1,26 @@
 import numpy as np
-import scipy.ndimage as ndi
-
-def _calc_vert(img, col, src_row, dst_row, linewidth):
-    # Quick calculation if perfectly vertical
-    pixels = img[src_row:dst_row:np.sign(dst_row - src_row),
-                 col - linewidth / 2: col + linewidth / 2 + 1]
-    return pixels.mean(axis=1)[..., np.newaxis]
+import scipy.ndimage as nd
 
 
-def profile_line(img, src, dst, linewidth=1, mode='constant', cval=0.0):
+def profile_line(img, src, dst, linewidth=1,
+                 order=1, mode='constant', cval=0.0):
     """Return the intensity profile of an image measured along a scan line.
 
     Parameters
     ----------
-    img : 2d or 3d array
-        The image, in grayscale (2d) or multichannel (2d + c) format.
+    img : numeric array, shape (M, N[, C])
+        The image, either grayscale (2D array) or multichannel 
+        (3D array, where the final axis contains the channel
+        information).
     src : 2-tuple of numeric scalar (float or int)
         The start point of the scan line.
     dst : 2-tuple of numeric scalar (float or int)
         The end point of the scan line.
     linewidth : int, optional
         Width of the scan, perpendicular to the line
+    order : int in {0, 1, 2, 3, 4, 5}, optional
+        The order of the spline interpolation to compute image values at
+        non-integer coordinates. 0 means nearest-neighbor interpolation.
     mode : string, one of {'constant', 'nearest', 'reflect', 'wrap'}, optional
         How to compute any values falling outside of the image.
     cval : float, optional
@@ -42,42 +42,44 @@ def profile_line(img, src, dst, linewidth=1, mode='constant', cval=0.0):
            [1, 1, 1, 2, 2, 2],
            [1, 1, 1, 2, 2, 2],
            [0, 0, 0, 0, 0, 0]])
-    >>> profile_line(img, (2, 1), (2, 5))
+    >>> profile_line(img, (2, 1), (2, 4))
     array([ 1.,  1.,  2.,  2.])
+
+    Notes
+    -----
+    The destination point is included in the profile, in contrast to
+    standard numpy indexing.
     """
     src_row, src_col = src = np.asarray(src, dtype=float)
     dst_row, dst_col = dst = np.asarray(dst, dtype=float)
     d_row, d_col = dst - src
-
-    # Quick calculation if perfectly vertical; shortcuts div0 error
-    if src_col == dst_col:
-        if img.ndim == 2:
-            img = img[:, :, np.newaxis]
-        img = np.rollaxis(img, -1)
-        intensities = np.hstack([_calc_vert(im, src_col, 
-                                            src_row, dst_row, linewidth)
-                                 for im in img])
-        return np.squeeze(intensities)
-
     theta = np.arctan2(d_row, d_col)
-    a = d_row / d_col
-    b = src_row - a * src_col
-    length = np.hypot(d_row, d_col)
 
-    line_x = np.linspace(src_col, dst_col, np.ceil(length))
-    line_y = line_x * a + b
-    y_width = abs(linewidth * np.cos(theta) / 2)
-    perp_ys = np.array([np.linspace(yi - y_width,
-                                    yi + y_width, linewidth) for yi in line_y])
-    perp_xs = - a * perp_ys + (line_x + a * line_y)[:, np.newaxis]
+    length = np.ceil(np.hypot(d_row, d_col) + 1)
+    # we add one above because we include the last point in the profile
+    # (in contrast to standard numpy indexing)
+    line_col = np.linspace(src_col, dst_col, length)
+    line_row = np.linspace(src_row, dst_row, length)
 
-    perp_lines = np.array([perp_ys, perp_xs])
+    # we subtract 1 from linewidth to change from pixel-counting
+    # (make this line 3 pixels wide) to point distances (the
+    # distance between pixel centers)
+    col_width = (linewidth - 1) * np.sin(-theta) / 2
+    row_width = (linewidth - 1) * np.cos(theta) / 2
+    perp_rows = np.array([np.linspace(row_i - row_width, row_i + row_width,
+                                      linewidth) for row_i in line_row])
+    perp_cols = np.array([np.linspace(col_i - col_width, col_i + col_width,
+                                      linewidth) for col_i in line_col])
+    perp_lines = np.array([perp_rows, perp_cols])
+
     if img.ndim == 3:
-        pixels = [ndi.map_coordinates(img[..., i], perp_lines, mode=mode,
-                                      cval=cval) for i in range(img.shape[2])]
+        pixels = [nd.map_coordinates(img[..., i], perp_lines,
+                                     order=order, mode=mode, cval=cval)
+                  for i in range(img.shape[2])]
         pixels = np.transpose(np.asarray(pixels), (1, 2, 0))
     else:
-        pixels = ndi.map_coordinates(img, perp_lines, mode=mode, cval=cval)
+        pixels = nd.map_coordinates(img, perp_lines,
+                                    order=order, mode=mode, cval=cval)
 
     intensities = pixels.mean(axis=1)
 

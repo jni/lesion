edad01902f8c9d23da106c538d118e28da286821
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-05-28 23:48:06 +1000

    Add function to determine metadata length

--- lesion/lifio.py
+++ lesion/lifio.py
@@ -1,3 +1,4 @@
+import numpy as np
 import javabridge as jv
 import bioformats as bf
 
@@ -20,3 +21,30 @@ def done():
     See the python-javabridge documentation for more information.
     """
     jv.kill_vm()
+
+
+def lif_metadata_string_size(filename):
+    """Get the length in bytes of the metadata string of a LIF file.
+
+    Parameters
+    ----------
+    filename : string
+        Path to the LIF file.
+
+    Returns
+    -------
+    length : int
+        The length in bytes of the metadata string.
+
+    Notes
+    -----
+    This is based on code by Lee Kamentsky. [1]
+
+    References
+    ----------
+    [1] https://github.com/CellProfiler/python-bioformats/issues/8
+    """
+    with open(filename, 'rb') as fd:
+        fd.read(9)
+        length = np.frombuffer(fd.read(4), "<i4")[0]
+        return length

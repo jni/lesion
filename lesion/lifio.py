f435dececa4a315a5e13d929709c063f48af27ee
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-05-28 22:46:41 +1000

    Start work on bioformats wrappers

---
+++ lesion/lifio.py
@@ -0,0 +1,22 @@
+import javabridge as jv
+import bioformats as bf
+
+def start(max_heap_size='8G'):
+    """Start the Java Virtual Machine, enabling bioformats IO.
+
+    Parameters
+    ----------
+    max_heap_size : string, optional
+        The maximum memory usage by the virtual machine. Valid strings
+        include '256M', '64k', and '2G'. Expect to need a lot.
+    """
+    jv.start_vm(class_path=bf.JARS, max_heap_size=max_heap_size)
+
+def done():
+    """Kill the JVM. Once killed, it cannot be restarted.
+
+    Notes
+    -----
+    See the python-javabridge documentation for more information.
+    """
+    jv.kill_vm()

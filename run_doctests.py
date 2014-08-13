312f6d380ed81b420878bb32ea996fef14ba3f6d
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-08-13 12:17:34 +1000

    Add new modules to doctests

--- run_doctests.py
+++ run_doctests.py
@@ -1,6 +1,6 @@
 
 if __name__ == '__main__':
     import doctest
-    from lesion import trace
-    doctest.testmod(trace)
+    from lesion import lifio, stats, trace
+    map(doctest.testmod, [lifio, stats, trace])
 

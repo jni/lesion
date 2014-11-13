342def7af63cd641dc5601831681759782802f0b
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-11-13 11:30:42 +1100

    Select only specific series for traces dict

--- lesion/process.py
+++ lesion/process.py
@@ -38,15 +38,21 @@ def bad_image(im):
     return is_bad
 
 
-def traces_dict(fin, chan=0):
+def traces_dict(fin, series=None, chan=0, return_images=False):
     """From a LIF file, produce image series, traces, stats.
 
     Parameters
     ----------
     fin : string
         The input filename.
+    series : list of int, optional
+        Which series to process. ``None`` is interpreted as all
+        available series.
     chan : int, optional
         The channel containing the image to be traced.
+    return_images : bool, optional
+        If ``True``, the images underlying the statistics are returned as
+        part of the traces dictionary.
 
     Returns
     -------
@@ -58,6 +64,10 @@ def traces_dict(fin, chan=0):
         each position and statistic.
     """
     names, sizes, resolutions = lifio.metadata(fin)
+    if series is None:
+        series = range(len(names))
+    names, sizes, resolutions = map(lambda x: [x[i] for i in series],
+                                    (names, sizes, resolutions))
     rdr = lifio.image_reader(fin)
     all_times = _times(names)
     positions, times = zip(*map(lifio.parse_series_name, names))
@@ -67,14 +77,21 @@ def traces_dict(fin, chan=0):
                   np.float32(np.nan))
     statistics = pd.DataFrame(statistics, index=all_times,
                               columns=it.product(positions, all_stat_names))
+    image_series = lifio.series_iterator(rdr, series,
+                                         desired_order='tzcyx', c=chan)
 
-    for i, name in enumerate(names):
-        images = lifio.read_image_series(rdr, i, desired_order='cztyx', c=chan)
-        images2d = images.sum(axis=0) # squish z dimension
+    for i, (name, images) in enumerate(zip(names, image_series)):
+        images2d = np.squeeze(images.sum(axis=1, dtype=np.uint16)) # squash z
+        if images2d.ndim == 2:
+            images2d = images2d[np.newaxis, ...]
         position, times = lifio.parse_series_name(name)
-        traces[position]['times'] = times
+        if not traces.has_key(position):
+            traces[position] = {'times': [], 'traces': [], 'images': []}
         current_traces = map(trace.trace_profile, images2d)
-        traces[position]['traces'] = current_traces
+        traces[position]['times'].extend(times)
+        traces[position]['traces'].extend(current_traces)
+        if return_images:
+            traces[position]['images'].extend(images2d)
         for tr, time in zip(current_traces, times):
             for stat, stat_name in zip(all_stats, all_stat_names):
                 statistics.loc[time][(position, stat_name)] = stat(tr)
@@ -105,4 +122,4 @@ def _times(names):
     positions, times = zip(*map(lifio.parse_series_name, names))
     start_times = [time[0] for time in times]
     _, indices = np.unique(start_times, return_index=True)
-    return np.concatenate([times[i] for i in indices])
+    return np.unique(np.concatenate([times[i] for i in indices]))
